eda2eb30235cc40bfe68985cf95311006f3019f6
DouglasGuacaran <douglasguacaran@gmail.com>
2023-09-05 08:35:20 -0300

    Agrego variables globales

--- ToDo-App/scripts/signup.js
+++ ToDo-App/scripts/signup.js
@@ -1,11 +1,37 @@
 window.addEventListener('load', function () {
     /* ---------------------- obtenemos variables globales ---------------------- */
-   
-
-
-    
+    const form =document.forms[0]
+    const nombre=form['inputNombre']
+    const apellido=form['inputApellido']
+    const email=form['inputEmail']
+    const password = form['inputPassword']
+    const passwordRepeated = form['inputPasswordRepetida']
+    const url = "https://todo-api.ctd.academy/v1"
+    console.log(form);
 
     /* -------------------------------------------------------------------------- */
+    /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
+    /* -------------------------------------------------------------------------- */
+    form.addEventListener('submit', function (event) {
+        event.preventDefault()  
+
+    const payload = {
+        nombre: nombre.value,
+        apellido : apellido.value ,
+        email: email.value,
+        password: password.value
+    }
+
+    const settings = {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+
+    });/* -------------------------------------------------------------------------- */
     /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
     /* -------------------------------------------------------------------------- */
     form.addEventListener('submit', function (event) {
